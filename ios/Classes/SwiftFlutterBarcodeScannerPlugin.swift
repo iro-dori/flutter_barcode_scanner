39b12f6b8b342b8e0504e4845ae7346348be9d09
Kato Hiroyuki <kato@iro-d-ori.co.jp>
2020-09-16 23:20:46 +0900

    japanese

--- ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
+++ ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
@@ -111,13 +111,13 @@ public class SwiftFlutterBarcodeScannerPlugin: NSObject, FlutterPlugin, ScanBarc
                                 
                             }
                         } else {
-                            let alert = UIAlertController(title: "Action needed", message: "Please grant camera permission to use barcode scanner", preferredStyle: .alert)
+                            let alert = UIAlertController(title: "アクセスの許可が必要です", message: "QRコードを読み取るためにはこのアプリのカメラへのアクセスを許可する必要があります", preferredStyle: .alert)
                             
-                            alert.addAction(UIAlertAction(title: "Grant", style: .default, handler: { action in
+                            alert.addAction(UIAlertAction(title: "許可する", style: .default, handler: { action in
                                 UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
                             }))
                             
-                            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                            alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel))
                             
                             SwiftFlutterBarcodeScannerPlugin.viewController.present(alert, animated: true)
                         }
